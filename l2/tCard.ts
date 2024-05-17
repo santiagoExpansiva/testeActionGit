69f61da27e714f6dfe5fa4a691096cd8e0cfcd67
Guilherme Santiago Silva <santiago@expansiva.com.br>
2024-05-17 09:28:59 -0300

    Teste compile 2

--- l2/tCard.ts
+++ l2/tCard.ts
@@ -58,7 +58,7 @@ class _200001_tCard extends _200001_Special_Base {
 
 	public render(): HTMLElement {
 
-		super.createEL('t-card-2');
+		super.createEL('t-card-3');
 		this.renderAllFields(this.el, this);
 		return this.el;
 
