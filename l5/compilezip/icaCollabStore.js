76c37bbfc595b0f2e16547eb573ae46b90745fc5
Guilherme Santiago Silva <santiago@expansiva.com.br>
2024-05-21 15:03:14 -0300

    Teste build js 3

--- l5/compilezip/icaCollabStore.js
+++
@@ -1,4 +0,0 @@
-const CHANGESTATE = "changeState";
-export {
-  CHANGESTATE
-};
--- l5/compilezip/icaLayoutFlowColumnBase.js
+++
@@ -1,6 +0,0 @@
-import { IcaLitElement } from "./_100554_icaLitElement";
-class IcaLayoutFlowColumnBase extends IcaLitElement {
-}
-export {
-  IcaLayoutFlowColumnBase
-};
